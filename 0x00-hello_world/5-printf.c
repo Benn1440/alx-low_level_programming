b2e8ff544b3e0fa73b1f82c83da30642c8d38604
Chinonso <benokafor67@gmail.com>
2022-03-11 01:03:03 +0100

    c lang

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,10 @@
 
 /**
  * main - Entry point
+ *  
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -9,3 +13,4 @@ int main(void)
     printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
+
